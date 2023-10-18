184f94f1a613fdc279ea527be1f68d1a6cf7bfef
huxulm <xulingming@xao.ac.cn>
2023-10-18 17:43:20 +0800

    update p223

--- p0223/main.go
+++ p0223/main.go
@@ -11,28 +11,15 @@ func solve(_r io.Reader, _w io.Writer) {
 	reader, writer := bufio.NewReader(_r), bufio.NewWriter(_w)
 	defer writer.Flush()
 
-	var mp [][]byte = [][]byte{
-		{'a','b','c'},
-		{'d','e','f'},
-		{'g','h','i'},
-		{'j','k','l'},
-		{'m','n','o'},
-		{'p','q','r'},
-		{'s','t'},
-		{'u','v'},
-		{'w','x'},
-		{'y','z'},
-	}
-
-	var s string
-	var v string
+	var mp []string = []string{"abc", "def", "ghi", "jkl", "mno", "pqr", "st", "uv", "wx", "yz"}
+	var s, v string
 
 	fmt.Fscan(reader, &s)
 	fmt.Fscan(reader, &v)
 
-	bans := [26]bool{}
+	bans := 0
 	for i := range v {
-		bans[v[i] - 'a'] = true
+		bans |= 1 << (v[i] - 'a')
 	}
 	seq := make([]int, len(s))
 	for i := range s {
@@ -40,27 +27,24 @@ func solve(_r io.Reader, _w io.Writer) {
 	}
 
 	var ans []string
-	var dfs func(i int)
+	var dfs func(i, mask int)
 	var perm []byte
 
-	dfs = func(i int) {
+	dfs = func(i, mask int) {
 		if i == len(seq) {
-			// check
-			bs := [26]bool{}
-			for j := range perm {
-				bs[perm[j] - 'a'] = true
-			}
-			if bs != bans {
+			if mask != bans {
 				ans = append(ans, string(append([]byte(nil), perm...)))
 			}
 			return
 		}
 		for _, c := range mp[seq[i]] {
-			perm = append(perm, c)
-			dfs(i + 1)
+			perm = append(perm, byte(c))
+			if (1 << (c - 'a')) & bans > 0 { mask |= 1 << (c - 'a') }
+			dfs(i + 1, mask)
+			if (1 << (c - 'a')) & bans > 0 { mask ^= 1 << (c - 'a') }
 			perm = perm[:len(perm) - 1]
 		}
 	}
-	dfs(0)
+	dfs(0, 0)
 	fmt.Fprintln(writer, strings.Join(ans, ","))
 }
